cbdafb99a9748aeb6a539a12a7e499e2b1b4b616
Harshavardhan Reddy <Harshavardhan.Reddy@Mercator.com>
2018-06-19 17:16:30 +0400

    Test

---
+++ src/inte/PrintBySpace.java
@@ -0,0 +1,15 @@
+package inte;
+
+public class PrintBySpace {
+
+	public static void main(String[] args) {
+		String s = "Harsha";
+	    String [] h = 	s.split("");
+	
+	for(int i = 0;i<7;i++){
+		System.out.print(h[i]+ " ");
+	}
+
+	}
+
+}
